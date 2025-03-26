2aebd5fcf02115f526a2b6b7e7df4954de749bee
Hamza7655code <mughis.rehman@yahoo.com>
2025-03-26 17:51:27 +0000

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,6 @@
+string = int(input("Please enter in your own string: "))
+string2 = ("")
+for i in string:
+    string2 = i + string2
+print("The original string = " , string)
+print("The reversed string = " , string2)
